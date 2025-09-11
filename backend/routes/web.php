4eec5ee7975b9dd1f44052891bb61e6adad1471d
Asun Fadrianto <asunfadrianto2799@gmail.com>
2025-09-11 22:22:24 +0700

    -

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -2,16 +2,26 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Response;
-use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\File;
 
-Route::get('/storage/{path}', function ($path) {
-    $file = storage_path('app/public/' . $path);
+Route::get('/frontend/{file}', function ($file) {
+    $path = public_path("frontend/" . $file);
 
-    if (!file_exists($file)) {
+    if (!file_exists($path)) {
         abort(404);
     }
 
-    return Response::file($file, [
-        'Access-Control-Allow-Origin' => '*', // Izinkan semua origin
-    ]);
-})->where('path', '.*');
+    $ext = pathinfo($path, PATHINFO_EXTENSION);
+
+    $contentType = 'text/plain';
+    if ($ext === 'js') {
+        $contentType = 'application/javascript; charset=UTF-8';
+    } elseif ($ext === 'css') {
+        $contentType = 'text/css; charset=UTF-8';
+    } elseif ($ext === 'html') {
+        $contentType = 'text/html; charset=UTF-8';
+    }
+
+    return response(File::get($path), 200)
+        ->header('Content-Type', $contentType);
+});
