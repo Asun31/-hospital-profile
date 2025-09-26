906ebda05b8b0cd5659e0506afaeb15558fbccb4
Asun Fadrianto <asunfadrianto2799@gmail.com>
2025-09-26 15:18:02 +0700

    -

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Masuk ke folder backend
-cd backend
-
-# Gunakan environment production
-cp .env.production .env
-
-# Jalankan Laravel di port yang diberikan Railway
-php artisan serve --host=0.0.0.0 --port=$PORT
